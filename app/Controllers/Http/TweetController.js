0bd56086bed59bb18724eca2b0d38056dff953fa
Guilherme Eiti Akita Miua <guilhermemiua@gmail.com>
2019-11-06 20:40:15 -0300

    Add tweet relations

--- app/Controllers/Http/TweetController.js
+++ app/Controllers/Http/TweetController.js
@@ -20,7 +20,10 @@ class TweetController {
    * @param {View} ctx.view
    */
   async index() {
-    const tweets = await Tweet.all()
+    const tweets = await Tweet
+      .query()
+      .with('user')
+      .fetch()
 
     return tweets
   }
